f5a905c4a56a1e24d010cd86d2ee0cc8c468947a
Andreas Hellmann <an-hellmann@gmx.de>
2016-11-16 11:12:49 +0100

    stash hard

--- www/js/app.js
+++ www/js/app.js
@@ -44,7 +44,7 @@ angular.module('starter', ['ionic', 'ngCordovaBeacon'])
     };
 
     $scope.onJawboneClick = function() { //https://vita-cena.mybluemix.net/redirect
-      
+
     };
 
     $ionicPlatform.ready(function() {
@@ -55,9 +55,31 @@ angular.module('starter', ['ionic', 'ngCordovaBeacon'])
 
             $scope.beacons = {};
 
+            var nearestBeacon = {};
+
             for(var i = 0; i < pluginResult.beacons.length; i++) {
                 var uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
                 $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+
+                if(!(typeof pluginResult.beacons[i].rssi === 'undefined' || pluginResult.beacons[i].rssi === null)
+                && ((typeof nearestBeacon.rssi === 'undefined' || nearestBeacon.rssi === null)
+                || nearestBeacon.rssi < pluginResult.beacons[i].rssi)){
+                  nearestBeacon = pluginResult.beacons[i];
+                }
+            }
+
+            if(nearestBeacon != null) {
+              $scope.testResult = JSON.stringify(nearestBeacon);
+              $http.get('https://vita-cena.mybluemix.net/meals?id=' + nearestBeacon.minor)
+                .success(function(data, status, headers,config){
+                  $scope.result = "";
+                  console.log('data success');
+                  console.log(JSON.stringify(data[0])); // for browser console
+                  $scope.result = data[0];
+                })
+                .error(function(data, status, headers,config){
+                  console.log('data error: ' + JSON.stringify(status) + ' | ' + JSON.stringify(headers));
+                });
             }
 
             $scope.$apply();
